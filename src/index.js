06866eaf309218ba627167315415581c89c33005
unknown <katherinne.g@hotmail.com>
2021-02-15 10:01:07 -0300

    IsValid Listo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import validator from './validator.js';
 
-console.log(validator);
+const bttnValidar = document.getElementById ("validarTC");
+
+bttnValidar.addEventListener ("click", () => {
+
+    const isValid =  validator.isValid(numberTC.value)
+    const masquify = validator.masquify (numberTC.value)
+    
+
+    if (isValid===true) {
+        alert ("Tarjeta " + masquify + " valida " );    
+    }
+    else {
+        alert ("Tarjeta invalida " + masquify);
+    }
+
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,36 @@
 const validator = {
-  // ...
+
+  isValid:(numberTC)=>{
+    let convertir = numberTC.split('').reverse()    
+    let suma = 0;
+    let n = 0;
+  for(let i=0; i<convertir.length; i++){
+    if(i%2 === 1){
+      n= Number(convertir[i])*2;
+      if(n>=10){
+        n= (n-10)+1  
+      };
+  } else{
+    n= Number(convertir[i])
+    };
+  suma = suma + n
+  }; 
+  return suma % 10 === 0 ? true : false
+  },
+  
+
+  masquify:(numberTC)=>{
+    let masquifyNumber = numberTC.split(''); console.log (masquifyNumber)
+    let replacement = "";
+  for(let i = 0; i<numberTC.length;i++){
+    if (i <=12){
+      replacement = masquifyNumber.replacement ( i,"#")
+    };
+    
+  };  
+}
+
+
 };
 
 export default validator;
